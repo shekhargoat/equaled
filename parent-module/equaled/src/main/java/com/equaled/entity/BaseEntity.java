6237603292768a2ee074ce9b94b9c4d7ac4923f9
Shirshendu <shirshendu@callcomm.com>
2024-03-27 14:44:32 +0530

    Small Issuue

--- parent-module/equaled/src/main/java/com/equaled/entity/BaseEntity.java
+++ parent-module/equaled/src/main/java/com/equaled/entity/BaseEntity.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 public abstract class BaseEntity implements Serializable{
 	
 	private static final long serialVersionUID = 7635914792695513185L;
-˚
+
 	final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 	
 	@Id
